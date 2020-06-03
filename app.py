8f846e1f0a64447623726e3721c34ba32b50e797
Eric LaFlure <ejlaflure@gmail.com>
2020-06-03 14:41:28 -0500

    adding scraping and app python files

---
+++ app.py
@@ -0,0 +1,29 @@
+# Import Dependancies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+# Set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Flask route for the index HTML page.
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+# Flask Srapping Route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+# Running Flask App
+if __name__ == "__main__":
+   app.run()
